e3077e24f403e512960b93c7686c9bcd8faef99c
shadi <shadi@noumenadigital.com>
2019-11-17 23:29:34 +0100

    DB & NodeJS

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-console.log('Hello from Node.js');
-console.log('update 1');
+var express = require('express');
+var bodyParser = require('body-parser');
+var sqlite = require('sqlite3');
+
+var app = express();
+var db = new sqlite.Database('projectDatabase.db');
+
+// serialize() ensures that statements are executed one after the other
+db.serialize(function () {
+// this creates a table
+    db.run('CREATE TABLE Contacts first_name TEXT, last_name TEXT, age INTEGER');
+
+    //insert values in the table 'Contacts'
+    db.run('INSERT INTO Contacts VALUES ("John", "Doe", 25)')
+});
+db.close();
