f36021ce9eb92c93b682e99b3ed9a38e34c7386e
Shadi <46449873+shadyels@users.noreply.github.com>
2019-11-19 22:41:50 +0100

    1st implementation of quotes with DB

---
+++ quotes-advanced/server.js
@@ -0,0 +1,34 @@
+var sqlite3 = require('sqlite3');
+var app = require('express');
+
+var db = new sqlite3.Database('quotes.db');
+
+
+//everything down here was pre-made, re-do it yourself at some point
+app.get('/quotes', function(req, res){
+    if(req.query.year){
+        db.all('SELECT * FROM quotes WHERE year = ?', [req.query.year], function(err, rows){
+            if(err){
+                res.send(err.message);
+            }
+            else{
+                console.log("Return a list of quotes from the year: " + req.query.year);
+                res.json(rows);
+            }
+        });
+    }
+    else{
+        db.all('SELECT * FROM quotes', function processRows(err, rows){
+            if(err){
+                res.send(err.message);
+            }
+            else{
+                for( var i = 0; i < rows.length; i++){
+                    console.log(rows[i].quote);
+                }
+                res.json(rows);
+            }
+        });
+    }
+});
+
